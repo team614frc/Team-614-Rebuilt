34cafb93e32e0eeb865f0444f1a689b33cf3da93
Lucas Nguyen <duyanlucasnguyen@gmail.com>
2026-02-03 16:55:01 -0500

    fix: intake constants

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -36,7 +36,7 @@ public class Intake extends SubsystemBase {
 
   private static final Voltage MAX_VOLTAGE = Volts.of(12.0);
   private static final Voltage MOTION_MAGIC_VOLTAGE = Volts.of(0);
-  private static final Voltage ROLLER_VOLTAGE_REQUEST = Volts.of(0);
+  //private static final Voltage ROLLER_VOLTAGE_REQUEST = Volts.of(0);
   private static final Angle PIVOT_REDUCTION = Degrees.of(50.0);
   private static final double PIVOT_PERCENT_OUTPUT = 0.1;
   private static final Current STATOR_CURRENT_LIMIT = Amps.of(120);
@@ -86,8 +86,8 @@ public class Intake extends SubsystemBase {
   private final TalonFX pivotMotor, rollerMotor;
   private final VoltageOut pivotVoltageRequest = new VoltageOut(Volts.of(0));
   private final MotionMagicVoltage pivotMotionMagicRequest =
-      new MotionMagicVoltage(MOTION_MAGIC_VOLTAGE.in(Volts)).withSlot(NEW_SLOT);
-  private final VoltageOut rollerVoltageRequest = new VoltageOut(ROLLER_VOLTAGE_REQUEST);
+      new MotionMagicVoltage(0).withSlot(0);
+  private final VoltageOut rollerVoltageRequest = new VoltageOut(Volts.of(0));
 
   private boolean isHomed = false;
 
