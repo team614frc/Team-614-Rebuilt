3196a4f7338bf7e75a7ee4f90e556aa1ea9cf5c1
Aaron <moonwatcher635@gmail.com>
2026-02-02 21:08:08 -0500

    fix: cleaned intake constants

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -46,7 +46,8 @@ public class Intake extends SubsystemBase {
   private static final Current STATOR_CURRENT_LIMIT = Amps.of(120);
   private static final Current SUPPLY_CURRENT_LIMIT = Amps.of(70);
   private static final Current HOMING_CURRENT_THRESHOLD = Amps.of(6);
-  private static final AngularVelocity kMaxPivotSpeed = KrakenX60.kFreeSpeed.div(PIVOT_REDUCTION.in(Degrees));
+  private static final AngularVelocity kMaxPivotSpeed =
+      KrakenX60.kFreeSpeed.div(PIVOT_REDUCTION.in(Degrees));
   private static final Angle kPositionTolerance = Degrees.of(5);
   private static final double KP = 300.0;
   private static final double KI = 0.0;
