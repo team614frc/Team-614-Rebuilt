d8c90950c55e0153ec1e6194182467bd79584c81
Aaron <moonwatcher635@gmail.com>
2026-02-06 15:47:22 -0500

    chore: spotless

--- src/main/java/frc/robot/subsystems/LEDs.java
+++ src/main/java/frc/robot/subsystems/LEDs.java
@@ -1,44 +1,44 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.ctre.phoenix.led.CANdle;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class LEDs extends SubsystemBase {
 
-    private final CANdle candle;
+  private final CANdle candle;
 
-    public LEDs() {
-        // CHANGE CAN ID LATER
-        candle = new CANdle(0);
-    }
+  public LEDs() {
+    // CHANGE CAN ID LATER
+    candle = new CANdle(0);
+  }
 
-    public void hubActive() {
+  public void hubActive() {
     // Solid green = OUR HUB ACTIVE
     candle.setLEDs(0, 255, 0);
-}
+  }
 
-public void opponentHub() {
+  public void opponentHub() {
     // Solid red = OPPONENT HUB ACTIVE
     candle.setLEDs(255, 0, 0);
-}
+  }
 
-public void hubStartingSoon() {
+  public void hubStartingSoon() {
     // Yellow = 5 second warning before OUR hub
     candle.setLEDs(255, 255, 0);
-}
+  }
 
-public void hubEndingSoon() {
+  public void hubEndingSoon() {
     // Purple = 5 second warning before OUR hub ends
     candle.setLEDs(160, 0, 255);
-}
+  }
 
-public void transition() {
+  public void transition() {
     // Blue during transition
     candle.setLEDs(0, 0, 255);
-}
+  }
 
-public void disabled() {
+  public void disabled() {
     // Dim white when disabled
     candle.setLEDs(40, 40, 40);
-}
+  }
 }
