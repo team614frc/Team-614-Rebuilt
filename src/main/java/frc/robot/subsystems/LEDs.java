2d1a38204cb3b940ab3256b112bb42f02a45b673
Aaron <moonwatcher635@gmail.com>
2026-02-06 17:23:25 -0500

    fix: updated deprecated code

--- src/main/java/frc/robot/subsystems/LEDs.java
+++ src/main/java/frc/robot/subsystems/LEDs.java
@@ -1,6 +1,8 @@
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.led.CANdle;
+import com.ctre.phoenix6.controls.SolidColor;
+import com.ctre.phoenix6.hardware.CANdle;
+import com.ctre.phoenix6.signals.RGBWColor;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class LEDs extends SubsystemBase {
@@ -9,36 +11,41 @@ public class LEDs extends SubsystemBase {
 
   public LEDs() {
     // CHANGE CAN ID LATER
-    candle = new CANdle(0);
+    candle = new CANdle(0, "rio");
+  }
+
+  private SolidColor SolidColorWithColor(int red, int green, int blue) {
+    RGBWColor color = new RGBWColor(red, green, blue);
+    return new SolidColor(8, 399).withColor(color);
   }
 
   public void hubActive() {
     // Solid green = OUR HUB ACTIVE
-    candle.setLEDs(0, 255, 0);
+    candle.setControl(SolidColorWithColor(0, 255, 0));
   }
 
   public void opponentHub() {
     // Solid red = OPPONENT HUB ACTIVE
-    candle.setLEDs(255, 0, 0);
+    candle.setControl(SolidColorWithColor(255, 0, 0));
   }
 
   public void hubStartingSoon() {
     // Yellow = 5 second warning before OUR hub
-    candle.setLEDs(255, 255, 0);
+    candle.setControl(SolidColorWithColor(255, 255, 0));
   }
 
   public void hubEndingSoon() {
     // Purple = 5 second warning before OUR hub ends
-    candle.setLEDs(160, 0, 255);
+    candle.setControl(SolidColorWithColor(160, 0, 255));
   }
 
   public void transition() {
     // Blue during transition
-    candle.setLEDs(0, 0, 255);
+    candle.setControl(SolidColorWithColor(0, 0, 255));
   }
 
   public void disabled() {
     // Dim white when disabled
-    candle.setLEDs(40, 40, 40);
+    candle.setControl(SolidColorWithColor(40, 40, 40));
   }
 }
