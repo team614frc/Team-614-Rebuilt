beacbeb3582321efdd754c3275bb006577e8564c
Lucas Nguyen <duyanlucasnguyen@gmail.com>
2026-02-20 16:18:39 -0500

    feat: led shift

--- src/main/java/frc/robot/subsystems/LEDs.java
+++ src/main/java/frc/robot/subsystems/LEDs.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems;
 
+import java.awt.Color;
+
 import com.ctre.phoenix6.controls.SolidColor;
 import com.ctre.phoenix6.hardware.CANdle;
 import com.ctre.phoenix6.signals.RGBWColor;
@@ -12,16 +14,18 @@ public class LEDs extends SubsystemBase {
     // CHANGE CAN ID LATER
     candle = new CANdle(31, "rio");
   }
-
+// WS2812B LEDs swapped
   public void setState(HubState state) {
-    candle.setControl(new SolidColor(8, 399).withColor(state.color));
+    candle.setControl(new SolidColor(0, 7).withColor(state.color));
+    RGBWColor color = new RGBWColor(state.color.Red, state.color.Green, state.color.Blue);
+    candle.setControl(new SolidColor(8, 399).withColor(color));
   }
 
   public enum HubState {
-    HUB_ACTIVE(0, 255, 0), // Green
+    HUB_ACTIVE(35, 220, 0), // Green
     OPPONENT_HUB(255, 0, 0), // Red
     HUB_STARTING_SOON(255, 255, 0), // Yellow
-    HUB_ENDING_SOON(160, 0, 255), // Purple
+    HUB_ENDING_SOON(190, 0, 255), // Purple
     TRANSITION(0, 0, 255), // Blue
     DISABLED(40, 40, 40); // Dim white
 
